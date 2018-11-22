bc829d431f107f89999023ab354a97e62503b7be
wanamaker44 <wanamaker44@gmail.com>
2018-11-22 15:11:18 -0500

    Added database connection.

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
 const express = require('express');
+const { Pool, Client } = require('pg')
 var app = express();
 app.use(express.static('static'));
-
-ibOne = {text:"This is ibOne", creator: "wwanamaker"};
-ibTwo = {text:"This is ibTwo", creator: "wwanamaker"};
-ibThree = {text:"This is ibThree", creator: "wwanamaker"};
-ibFour = {text:"This is ibFour", creator: "wwanamaker"}
-iceBreakers = [ibOne, ibTwo, ibThree, ibFour];
+var ibIndex;
+var pool;
 
 app.get('/', (req, res) => {
-    res.sendFile('/static/index.html', {root: __dirname });
+    res.sendFile('static/index.html', {root: __dirname });
 });
 
 app.get('/randomicebreaker', (req, res) => {
-	var ibIndex = Math.floor(Math.random() * 4);
-
-	// create call to DB for record. Get count first I guess then randomize that number
-
-	res.send(iceBreakers[ibIndex]);
+	setupPool();
+	pool.query('SELECT count(*) from icebreakers', function(err, result) {
+		ibIndex = result.rows[0].count;
+		ibIndex = Math.floor(Math.random() * ibIndex);
+		pool.query('SELECT * from icebreakers', function(err, result) {
+			res.send(result.rows[ibIndex]);
+			});
+		});
 });
 
+function setupPool() {
+	pool = new Pool({
+  		user: 'postgres',
+  		host: 'localhost',
+  		database: 'postgres',
+  		password: 'mysecretpassword',
+  		port: 5432
+	});
+}
+	
 app.listen(process.env.PORT || 3000)
